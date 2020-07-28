83552346a09c4542116e3d545e986992a7c82285
Siling <think.sea@gmail.com>
2020-07-28 23:23:45 +0800

    remove bs4!

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <style lang="scss">
-@import '~bootstrap/scss/bootstrap';
+// @import '~bootstrap/scss/bootstrap';
 #app {
   font-family: "Microsoft JhengHei", Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
