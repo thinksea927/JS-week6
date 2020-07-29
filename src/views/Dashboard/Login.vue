a75aac9001d43aea0222897259f52937c7d827a6
Siling <think.sea@gmail.com>
2020-07-29 10:12:10 +0800

    remove label for

--- src/views/Dashboard/Login.vue
+++ src/views/Dashboard/Login.vue
@@ -3,11 +3,11 @@
     <form @submit.prevent="signin">
       <h3>請登入</h3>
       <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
+          <!-- <label for="exampleInputEmail1">Email address</label> -->
           <input class="form-control" id="exampleInputEmail1" type="email" aria-describedby="emailHelp" placeholder="Enter email" v-model="user.email" requied="requied"/>
         </div>
         <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
+          <!-- <label for="exampleInputPassword1">Password</label> -->
           <input class="form-control" id="exampleInputPassword1" type="password" placeholder="Password" v-model="user.password" required="required"/>
         </div>
         <button class="btn btn-primary mr-3" type="submit">Log In</button>
