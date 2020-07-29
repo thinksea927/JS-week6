240cc495216b0af0e103ae32196060d3b7737558
Siling <think.sea@gmail.com>
2020-07-29 11:07:41 +0800

    update css style on form-group

--- src/views/Dashboard/Login.vue
+++ src/views/Dashboard/Login.vue
@@ -2,12 +2,12 @@
   <div>
     <form @submit.prevent="signin">
       <h3>請登入</h3>
-      <div class="form-group">
-          <label for="exampleInputEmail1" class="text-left">Email address</label>
+      <div class="form-group text-left">
+          <label for="exampleInputEmail1">Email address</label>
           <input class="form-control" id="exampleInputEmail1" type="email" aria-describedby="emailHelp" placeholder="Enter email" v-model="user.email" requied="requied"/>
         </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1" class="text-left">Password</label>
+        <div class="form-group text-left">
+          <label for="exampleInputPassword1">Password</label>
           <input class="form-control" id="exampleInputPassword1" type="password" placeholder="Password" v-model="user.password" required="required"/>
         </div>
         <button class="btn btn-primary mr-3" type="submit">Log In</button>
